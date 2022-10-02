f99db091e96d466e68d067bf6b51c755151bf5ca
xwj-june <xwj.june@gmail.com>
2022-10-02 14:36:56 +1300

    apply await async and start tasks concurrently

--- Asynchronous_Programming/Basic_Async/Program.cs
+++ Asynchronous_Programming/Basic_Async/Program.cs
@@ -3,8 +3,6 @@ using System.Threading.Tasks;
 
 namespace Basic_Async
 {
-
-
     internal class Bacon { }
     internal class Coffee { }
     internal class Egg { }
@@ -13,18 +11,25 @@ namespace Basic_Async
 
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            await BasicAsync(); //Execution Time: 15013 ms
+            await TaskConcurrentAsync(); //Execution Time: 6010 ms
+        }
+
+        private static async Task BasicAsync(){
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
             Coffee cup = PourCoffee();
             Console.WriteLine("coffee is ready");
 
-            Egg eggs = FryEggs(2);
+            Egg eggs = await FryEggsAsync(2);
             Console.WriteLine("eggs are ready");
 
-            Bacon bacon = FryBacon(3);
+            Bacon bacon = await FryBaconAsync(3);
             Console.WriteLine("bacon is ready");
 
-            Toast toast = ToastBread(2);
+            Toast toast = await ToastBreadAsync(2);
             ApplyButter(toast);
             ApplyJam(toast);
             Console.WriteLine("toast is ready");
@@ -32,6 +37,61 @@ namespace Basic_Async
             Juice oj = PourOJ();
             Console.WriteLine("oj is ready");
             Console.WriteLine("Breakfast is ready!");
+
+            watch.Stop();
+            Console.WriteLine($"BasicAsync Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+
+        private static async Task TaskConcurrentAsync(){
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            Coffee cup = PourCoffee();
+            Console.WriteLine("Coffee is ready");
+
+            Task<Egg> eggsTask = FryEggsAsync(2);
+            Task<Bacon> baconTask = FryBaconAsync(3);
+            Task<Toast> toastTask = MakeToastWithButterAndJamAsync(2);
+
+            #region await individual task manually
+            // Egg eggs = await eggsTask;
+            // Console.WriteLine("Eggs are ready");
+
+            // Bacon bacon = await baconTask;
+            // Console.WriteLine("Bacon is ready");
+
+            // Toast toast = await toastTask;
+            // Console.WriteLine("Toast is ready");
+            #endregion
+
+            #region await tasks using Task method
+            var breakfastTasks = new List<Task> { eggsTask, baconTask, toastTask };
+            while (breakfastTasks.Count > 0)
+            {
+                Task finishedTask = await Task.WhenAny(breakfastTasks);
+                if (finishedTask == eggsTask)
+                {
+                    Console.WriteLine("eggs are ready");
+                }
+                else if (finishedTask == baconTask)
+                {
+                    Console.WriteLine("bacon is ready");
+                }
+                else if (finishedTask == toastTask)
+                {
+                    Console.WriteLine("toast is ready");
+                }
+                breakfastTasks.Remove(finishedTask);
+            }
+            #endregion
+
+            Juice oj = PourOJ();
+            Console.WriteLine("Oj is ready");
+
+            Console.WriteLine("Breakfast is ready!");
+
+            watch.Stop();
+            Console.WriteLine($"TaskConcurrentAsync Execution Time: {watch.ElapsedMilliseconds} ms");
         }
 
         private static Juice PourOJ()
@@ -46,42 +106,51 @@ namespace Basic_Async
         private static void ApplyButter(Toast toast) =>
             Console.WriteLine("Putting butter on the toast");
 
-        private static Toast ToastBread(int slices)
+        private static async Task<Toast> ToastBreadAsync(int slices)
         {
             for (int slice = 0; slice < slices; slice++)
             {
                 Console.WriteLine("Putting a slice of bread in the toaster");
             }
             Console.WriteLine("Start toasting...");
-            Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             Console.WriteLine("Remove toast from toaster");
 
             return new Toast();
         }
 
-        private static Bacon FryBacon(int slices)
+        static async Task<Toast> MakeToastWithButterAndJamAsync(int number)
+        {
+            var toast = await ToastBreadAsync(number);
+            ApplyButter(toast);
+            ApplyJam(toast);
+
+            return toast;
+        }
+
+        private static async Task<Bacon> FryBaconAsync(int slices)
         {
             Console.WriteLine($"putting {slices} slices of bacon in the pan");
             Console.WriteLine("cooking first side of bacon...");
-            Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             for (int slice = 0; slice < slices; slice++)
             {
                 Console.WriteLine("flipping a slice of bacon");
             }
             Console.WriteLine("cooking the second side of bacon...");
-            Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             Console.WriteLine("Put bacon on plate");
 
             return new Bacon();
         }
 
-        private static Egg FryEggs(int howMany)
+        private static async Task<Egg> FryEggsAsync(int howMany)
         {
             Console.WriteLine("Warming the egg pan...");
-            Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             Console.WriteLine($"cracking {howMany} eggs");
             Console.WriteLine("cooking the eggs ...");
-            Task.Delay(3000).Wait();
+            await Task.Delay(3000);
             Console.WriteLine("Put eggs on plate");
 
             return new Egg();
