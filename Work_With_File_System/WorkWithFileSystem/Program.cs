8705dbb2e5f9d705c61fe1602ad0878e563a39f3
xwj-june <xwj.june@gmail.com>
2022-10-04 22:21:08 +1300

    list dir and file using Directory

---
+++ Work_With_File_System/WorkWithFileSystem/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//List all directories
+IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("stores");
+
+foreach (var dir in listOfDirectories) {
+    Console.WriteLine(dir);
+}
+
+//List files in a specific directory
+IEnumerable<string> files = Directory.EnumerateFiles("stores");
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+
+//List all content in a directory and all subdirectories
+    //Find all *.txt files in the stores folder and its subfolders
+IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles("stores", "*.txt", SearchOption.AllDirectories);
+
+foreach (var file in allFilesInAllFolders)
+{
+    Console.WriteLine(file);
+}
